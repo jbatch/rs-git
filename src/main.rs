7894d11657506404dfb88250c075adec6a1d6f31
Josh Batchelor <josh_batch@live.com.au>
2022-06-25 18:04:11 +0800

    Reimplement without clap

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,86 @@
-use clap::{Parser, Subcommand};
+// use clap::{Parser, Subcommand};
 use std::{
     error::Error,
     fmt,
     fs::{self, File},
-    io::{self, Read},
+    io::Read,
     path::Path,
 };
 
 use flate2::read::ZlibDecoder;
 
-#[derive(Subcommand, Debug, Clone)]
+#[derive(Debug, Clone)]
+// #[derive(Subcommand, Debug, Clone)]
 pub enum Command {
     Init {},
-    #[clap(name = "cat-file")]
+    // #[clap(name = "cat-file")]
     CatFile {
         /// show object type
-        #[clap(name = "type", short = 't', value_parser)]
+        // #[clap(name = "type", short = 't', value_parser)]
         print_type: bool,
-        #[clap(name = "size", short = 's', value_parser)]
+        // #[clap(name = "size", short = 's', value_parser)]
         /// show object size
         print_size: bool,
-        #[clap(short = 'p', value_parser)]
+        // #[clap(short = 'p', value_parser)]
         /// pretty print the object's contents
         pretty_print: bool,
         object: String,
     },
 }
 
-#[derive(Parser, Debug)]
-#[clap(author, version, about, long_about = None)]
+// #[derive(Parser, Debug)]
+#[derive(Debug)]
+// #[clap(author, version, about, long_about = None)]
 pub struct Args {
     /// Command to run
-    #[clap(subcommand)]
+    // #[clap(subcommand)]
     pub command: Command,
+}
+
+impl Args {
+    fn parse() -> Result<Args> {
+        let args = std::env::args();
+        // Skip executable name
+        let mut args = args.skip(1).peekable();
+
+        let command = match args.next() {
+            Some(command) => Ok(command),
+            None => Err(GitError::InvalidArgs()),
+        }?;
+
+        let command = match command.as_str() {
+            "init" => Ok(Command::Init {}),
+            "cat-file" => {
+                let print_size = false;
+                let print_type = false;
+                let mut pretty_print = false;
+                let mut object: Option<String> = None;
+                while let Some(arg) = args.peek() {
+                    if arg.starts_with("-") {
+                        if arg == "-p" {
+                            pretty_print = true;
+                        }
+                        args.next().unwrap();
+                    } else {
+                        // treat as positional arg <object>
+                        object = Some(args.next().unwrap());
+                    }
+                }
+                match object {
+                    Some(object) => Ok(Command::CatFile {
+                        print_type,
+                        print_size,
+                        pretty_print,
+                        object,
+                    }),
+                    None => Err(GitError::InvalidArgs()),
+                }
+            }
+            _ => Err(GitError::InvalidArgs()),
+        }?;
+
+        Ok(Args { command })
+    }
 }
 
 #[derive(Debug)]
@@ -42,6 +90,7 @@ pub enum Object {
 
 #[derive(Debug, Clone)]
 pub enum GitError {
+    InvalidArgs(),
     CorruptFile(),
 }
 
@@ -49,6 +98,7 @@ impl fmt::Display for GitError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             GitError::CorruptFile() => write!(f, "Could not read corrupted file"),
+            GitError::InvalidArgs() => write!(f, "Invalid command line args"),
         }
     }
 }
@@ -79,8 +129,8 @@ impl Object {
     }
 }
 
-fn main() {
-    let args = Args::parse();
+fn main() -> Result<()> {
+    let args = Args::parse()?;
 
     match args.command {
         Command::Init {} => {
@@ -98,6 +148,7 @@ fn main() {
             print_size,
         }),
     }
+    Ok(())
 }
 
 fn init() {
